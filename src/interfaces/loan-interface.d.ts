96c46bf2857cd997299772f4c6148c9a04a6fa29
TinTinWinata <86914748+TinTinWinata@users.noreply.github.com>
2023-07-29 03:03:56 +0700

    fix: merge loan interface

--- src/interfaces/loan-interface.d.ts
+++ src/interfaces/loan-interface.d.ts
@@ -5,7 +5,7 @@ export interface IRequestLoanForm {
 }
 
 export interface IRequestLoan {
-    prediction: number;
+  prediction: number;
 }
 
 export interface IAcceptLoan {
@@ -17,14 +17,6 @@ export interface IPayLoan {
 }
 
 export interface IGetLoan {
-<<<<<<< HEAD
-    loan_approval: IGetLoanApproval;
-    loan: {
-        id: string;
-        is_payed: boolean;
-        approval: string;
-    };
-=======
   loan_approval: {
     created_at: Date;
     id: string;
@@ -39,28 +31,27 @@ export interface IGetLoan {
     is_payed: boolean;
     approval: string;
   };
->>>>>>> 8d08541ccb549a6413ae5bbb95013a0d0ae6f31a
 }
 
 export interface IGetLoanApproval {
-    id: string;
-    user: number;
-    loan_amount: number;
-    loan_days_term: number;
-    receiverAccountNo: string;
-    is_approved: boolean | null;
-    create_at: string;
-    user_data: {
-        uid: string;
-        username: string;
-        email: string;
-        gender: 1;
-        role: string;
-        user_approval: 6;
-    };
+  id: string;
+  user: number;
+  loan_amount: number;
+  loan_days_term: number;
+  receiverAccountNo: string;
+  is_approved: boolean | null;
+  create_at: string;
+  user_data: {
+    uid: string;
+    username: string;
+    email: string;
+    gender: 1;
+    role: string;
+    user_approval: 6;
+  };
 }
 
 export interface IApproveLoanApprovalForm {
-    loan_approval_id: string;
-    user_id: number;
+  loan_approval_id: string;
+  user_id: number;
 }
